6e49dbfbf9857079e51b014331780237e6aaa2af
Avishka Hansana Bandara <avishkah.bandara@gmail.com>
2026-04-20 16:10:18 +0530

    change2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
+  site: "https://avishkabandara23.github.io",
+  base: "/portfolio",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
